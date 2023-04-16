91ab8d63d017ff33cb4c3eb7804dd96ef57156cd
Mathias Adam Møller <adam.and.math@gmail.com>
2023-04-16 20:42:27 +0200

    Initial commit

---
+++ src/cock.rs
@@ -0,0 +1,103 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct CustomSet {
+    pub sets: Sets,
+    pub cards: Cards,
+}
+
+#[derive(Deserialize)]
+pub struct CardNote {
+    #[serde(default)]
+    pub related: Vec<CardRelation>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Sets {
+    #[serde(default)]
+    pub set: Vec<Set>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Set {
+    pub name: String,
+    pub longname: String,
+    pub settype: String,
+    pub releasedate: Option<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Cards {
+    #[serde(default)]
+    pub card: Vec<Card>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Card {
+    pub name: String,
+    pub text: String,
+    pub prop: CardProp,
+    pub tablerow: Option<String>,
+    pub token: Option<String>,
+    #[serde(default)]
+    pub set: Vec<CardSet>,
+    #[serde(default)]
+    pub related: Vec<CardRelation>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct CardProp {
+    pub manacost: Option<String>,
+    pub side: Option<String>,
+    pub cmc: Option<String>,
+    pub layout: Option<String>,
+    #[serde(rename="type")]
+    pub ttype: String,
+    #[serde(rename="maintype")]
+    pub main_type: String,
+    #[serde(rename="coloridentity")]
+    pub color_identity: Option<String>,
+    pub colors: Option<String>,
+    pub pt: Option<String>,
+    pub loyalty: Option<String>,
+    #[serde(rename="format-commander")]
+    pub format_commander: Option<String>,
+    #[serde(rename="format-vintage")]
+    pub format_vintage: Option<String>,
+    #[serde(rename="format-legacy")]
+    pub format_legacy: Option<String>,
+    #[serde(rename="format-duel")]
+    pub format_duel: Option<String>,
+    #[serde(rename="format-oathbreaker")]
+    pub format_oathbreaker: Option<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct CardSet {
+    #[serde(rename="@uuid")]
+    pub uuid: Option<String>,
+    #[serde(rename="@num")]
+    pub num: Option<String>,
+    #[serde(rename="@rarity")]
+    pub rarity: Option<String>,
+    #[serde(rename="@muid")]
+    pub muid: Option<String>,
+    #[serde(rename="@picurl")]
+    pub picurl: Option<String>,
+    #[serde(rename="$text")]
+    pub name: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct CardRelation {
+    #[serde(rename="@count")]
+    pub count: Option<String>,
+    #[serde(rename="@attach")]
+    pub attach: Option<String>,
+    #[serde(rename="@exclude")]
+    pub exclude: Option<String>,
+    #[serde(rename="@persistent")]
+    pub persistent: Option<String>,
+    #[serde(rename="$text")]
+    pub name: String,
+}
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+mod cock;
+use cock::*;
+
+use std::collections::HashMap;
+
+use regex::Regex;
+
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let prefix = args.next().unwrap();
+    let from = args.next().unwrap();
+    let to = args.next().unwrap();
+    let txt_file = std::fs::read_to_string(format!("{from}.txt")).expect("Failed to read txt");
+    let xml_file = std::fs::read_to_string(format!("{from}.xml")).expect("Failed to read xml");
+    let notes = parse_notes(&txt_file);
+    let mut set : CustomSet = quick_xml::de::from_str(&xml_file).expect("Failed to read xml");
+
+    for card in &mut set.cards.card {
+        let name = if card.name.starts_with(&prefix) {
+            &card.name[prefix.len()..]
+        } else {
+            &card.name
+        };
+        let note = *notes.get(&*name).unwrap_or_else(||panic!("Failed to find note for `{}`", name));
+        let note: CardNote = quick_xml::de::from_str(note).unwrap_or_else(|e|panic!("Failed to parse note for `{}`: {:?}", name, e));
+        if !card.related.is_empty() {
+            panic!("`{}` has related cards", name);
+        }
+        card.related = note.related;
+    }
+
+    let output = quick_xml::se::to_string(&set).unwrap();
+    std::fs::write(format!("{to}.xml"), output).unwrap();
+}
+
+fn parse_notes<'a>(file: &'a str) -> HashMap<&'a str, &'a str> {
+    let name_regex = Regex::new(r"^(.*?)\[/b\]").unwrap();
+    let note_regex = Regex::new(r"\[spoiler\]Card Notes: (.*?)\[/spoiler\]").unwrap();
+    let mut map = HashMap::new();
+
+    for card in file.split("[b]").skip(1) {
+        let name: &'a str = &name_regex.captures(card).unwrap().get(1).unwrap().as_str();
+        let note: &'a str = note_regex.captures(card).map_or("</note>", |cap|cap.get(1).unwrap().as_str());
+        if map.insert(name, note).is_some() {
+            panic!("`{name}` is defined multiple times");
+        }
+    }
+
+    map
+}
